e02b1711e00bd62bd200cb20f5086fa2c0858dc3
xiaorangjun <137222705@qq.com>
2017-12-28 01:23:49 +0800

    gateway update

---
+++ rounaservice/rouna-gateway/src/main/java/com/rouna/zuul/MainZuulApplication.java
@@ -0,0 +1,16 @@
+package com.rouna.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by Administrator on 2017/12/28.
+ */
+@SpringBootApplication
+@EnableZuulProxy // 使用@EnableZuulProxy来开启Zuul的支持，如果你不想使用Zuul提供的Filter和反向代理的功能的话，此处可以使用@EnableZuulServer注解
+public class MainZuulApplication {
+    public static void main(String [] args){
+        SpringApplication.run(MainZuulApplication.class,args);
+    }
+}
